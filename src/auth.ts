1aa6ff66d8b605ff2cb8d14fb1391de232d26133
NABILAARA <nabilaara033@gmail.com>
2026-07-02 15:42:47 +0700

    Update auth.ts
    fix: tipe data adapter nextauth

--- src/auth.ts
+++ src/auth.ts
@@ -1,10 +1,11 @@
 import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
+import type { Adapter } from "next-auth/adapters";
 import { prisma } from "@/lib/db";
 import { authConfig } from "@/lib/auth/config";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
-  adapter: PrismaAdapter(prisma),
+  adapter: PrismaAdapter(prisma) as Adapter,
   session: { strategy: "jwt" },
   ...authConfig
 });
