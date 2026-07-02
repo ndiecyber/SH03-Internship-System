833ccebbde394348467639042003717974e9f29c
NABILAARA <nabilaara033@gmail.com>
2026-07-02 16:05:03 +0700

    Update auth.ts
    restore to origin

--- src/auth.ts
+++ src/auth.ts
@@ -1,11 +1,10 @@
 import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import type { Adapter } from "next-auth/adapters";
 import { prisma } from "@/lib/db";
 import { authConfig } from "@/lib/auth/config";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
-  adapter: PrismaAdapter(prisma) as Adapter,
+  adapter: PrismaAdapter(prisma),
   session: { strategy: "jwt" },
   ...authConfig
 });
